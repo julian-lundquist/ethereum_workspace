1cf62811bf5aed8fd5b825d8d831ece271cd1117
julian-lundquist <julian.j.lundquist@gmail.com>
2021-04-26 16:46:25 -0500

    added the ability to interact/compile solidity contracts

---
+++ compile.js
@@ -0,0 +1,38 @@
+const solc = require('solc');
+const fs = require('fs');
+const path = require('path');
+
+const fileName = 'Inbox.sol'
+const inboxPath = path.resolve(__dirname, 'contracts', fileName);
+const data = fs.readFileSync(inboxPath, 'utf8');
+
+var input = {
+    language: 'Solidity',
+    sources: {
+        'content.sol': {
+            content: data
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+};
+
+// console.log(solc.compile(JSON.stringify(input)))
+var output = JSON.parse(solc.compile(JSON.stringify(input, 1)));
+console.log(output)
+
+// `output` here contains the JSON output as specified in the documentation
+for (var contractName in output.contracts['content.sol']) {
+    console.log(
+        contractName +
+        ': ' +
+        output.contracts['content.sol'][contractName].evm.bytecode.object
+    );
+}
+
+module.exports = output;
